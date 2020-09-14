22f41c510d20e836b5ee16e976b6ab6abd8f754a
Zach Cullen <zacullen@gmail.com>
2020-09-14 14:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function findMindAndRemoveSorted(array) {
+  let min = array[0]
+  let minIndex = 0
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      minIndex = i
+    }
+  }
+  array.splice(minIndex, 1)
+  return min
+}
